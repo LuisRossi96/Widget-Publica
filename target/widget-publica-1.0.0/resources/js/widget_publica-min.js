7a08f58113e20bec442a31089b0d378f0006cd0e
Luis Felipe de Andrade Rossi <luis.rossi@sp01.local>
2020-03-16 11:15:03 -0300

    Atualização dos fontes

---
+++ target/widget-publica-1.0.0/resources/js/widget_publica-min.js
@@ -0,0 +1,1 @@
+var HelloWorld=SuperWidget.extend({message:null,init:function(){},bindings:{local:{"show-message":["click_showMessage"]}},showMessage:function(){$div=$("#helloMessage_"+this.instanceId);$message=$("<div>").addClass("message").append(this.message);$div.append($message)}});
